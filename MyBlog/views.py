464095841b72e4281756d18cfdff27421ed8e221
SJDream <SJ.Lee@outlook.com>
2015-07-06 23:00:06 +0800

    模板页修改
    模板页修改

--- MyBlog/views.py
+++ MyBlog/views.py
@@ -7,8 +7,9 @@ def index(request):
 	#return HttpResponse(u"欢迎来到SJ.Lee的避风港!")
 	stringWelcome = "欢迎来到SJ.Lee的避风港@!"
 	strMenu = ["主页","关于我","生活日记","工作心得","留言簿"]
+	strContract = {'TelPhone':u'18952367310','EMail':u'SJ.Lee@outlook.com'}
 	return render(request,'home.html'
-		,{'strWelcome':stringWelcome,'strMenu':strMenu})
+		,{'strWelcome':stringWelcome,'strMenu':strMenu,'strContract':strContract})
 
 def add(request):
 	a = request.GET['a']
